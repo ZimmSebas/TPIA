78a4717a47e0e822d6a2961302d16f1a8a72feac
Sebas <zimmermannsebas@gmail.com>
2020-05-06 11:49:41 -0300

    Add finalized TP2

--- TP_Prolog/tp2.pl
+++ TP_Prolog/tp2.pl
@@ -1,13 +1,33 @@
+
+:- dynamic si/1,no/1.
+sat(Atributo,_) :- (si(Atributo) -> true ; ( no(Atributo) -> fail ; pregunta(Atributo))).
+
+satIf(AtributoNO,AtributoSI,_) :- si(AtributoSI) -> true ; (no(AtributoSI) -> fail ; 
+    (si(AtributoNO) -> fail ; (no(AtributoNO) -> true ; pregunta(AtributoSI)))).
+
+% noSat(Atributo,_) :- (si(Atributo) -> fail ; (no(Atributo) -> true ; preguntaNO(Atributo))).
+
+pregunta(A) :- write('¿Are you looking for a language with the following property?: '), write(A), write(' '), nl, read(Resp), nl,
+((Resp == s ; Resp == si ; Resp == sí) -> assert(si(A)); assert(no(A)), fail).
+
+% preguntaNO(A) :- write('¿Are you looking for a language with the following property?: '), write(A), write(' '), nl, read(Resp), nl,
+% ((Resp == s ; Resp == si ; Resp == sí) -> assert(si(A)), fail; assert(no(A))).
+
 % typing
 
 ttyped(X) :- satIf(untyped, typed, X).
 tuntyped(X) :- satIf(typed, untyped, X).
 
-tdynamic(X) :- ttyped(X), si(staticTyped) -> false ; (no(staticTyped) -> true ; sat(dynamicTyped, X)).
-tstatic(X) :- ttyped(X), si(dynamicTyped)-> false ; (no(dynamicTyped) -> true ; sat(staticTyped, X)).
-tstrong(X) :- ttyped(X), si(weakTyped) -> false ; (no(weakTyped) -> true ; sat(strongTyped, X)).
-tweak(X) :- ttyped(X), si(strongTyped) -> false ; (no(strongTyped) -> true; sat(weakTyped, X)).
+tdynamic(X) :- ttyped(X), satIf(staticTyped, dynamicTyped, X).
+tstatic(X) :- ttyped(X), satIf(dynamicTyped, staticTyped, X).
+tstrong(X) :- ttyped(X), satIf(weaklyTyped, stronglyTyped, X).
+tweak(X) :- ttyped(X), satIf(stronglyTyped, weaklyTyped, X).
 
+% level
+
+lhigh(X) :- (si(lowLevel) -> fail ; si(mediumLevel) -> fail ; (no(lowLevel), no(mediumLevel)) -> true ; sat(highLevel,X)). 
+llow(X) :- (si(highLevel) -> fail ; si(mediumLevel) -> fail ; (no(highLevel), no(mediumLevel)) -> true ; sat(lowLevel,X)).
+lmedium(X) :- (si(highLevel) -> fail ; si(lowLevel) -> fail ; (no(highLevel), no(lowLevel)) -> true ; sat(mediumLevel,X)). 
 
 % paradigm
 
@@ -22,63 +42,52 @@ pure(X) :- pfunctional(X), noSat(imperative, X), noSat(logic, X), noSat(query, X
 
 % domain
 
-dgeneric(X) :- si(specificDomain) -> false ; (no(specificDomain) -> true ; sat(genericDomain, X)).
-dspecific(X) :- si(genericDomain) -> false ; (no(genericDomain) -> true ; sat(specificDomain, X)).
+dgeneric(X) :- satIf(specificDomain, genericDomain, X).
+dspecific(X) :- satIf(genericDomain, specificDomain, X).
 
 
-assembler(X) :- tuntyped(X), sat(low, X).
-python(X) :- sat(high, X), pfunctional(X), pobject(X), dgeneric(X), tdynamic(X), tstrong(X).
-c(x) :- sat(medium,X), tstatic(X), tweak(X), dgeneric(X), pprocedural(X).
-haskell(X) :- sat(high, X), tstatic(X), tstrong(X), dgeneric(X), pure(X).
-erlang(X) :- sat(high, X), tdynamic(X), tstrong(X), dgeneric(X), pfunctional(X).
-ocaml(X) :- sat(high, X), tstatic(X), tstrong(X), dgeneric(X), pfunctional(X), pobject(X).
-scilab(X) :- sat(high, X), tdynamic(X), tweak(X), dspecific(X), pfunctional(X), pobject(X), pprocedural(X).
-swiprolog(X) :- sat(high, X), tuntyped(X), dspecific(X), plogic(X).
-datalog(X) :- sat(high, X), tuntyped(X), dspecific(X), plogic(X), pquery(X).
-mysql(X) :- sat(high, X), tuntyped(X), dspecific(X), pquery(X).
-cpp(X) :- sat(medium, X), tstatic(X), tweak(X), dgeneric(X), pobject(X).
-r(X) :- sat(high, X), tdynamic(X), tweak(X), dspecific(X), pobject(X), pprocedural(X).
-java(X) :- sat(medium, X), tstatic(X), tstrong(X), dgeneric(X), pobject(X), pdeclarative(X).
-javascript(X) :- sat(high, X), tdynamic(X), tweak(X), dgeneric(X), pobject(X).
-ada(X) :- sat(high, X), tstatic(X), tstrong(X), dgeneric(X), pprocedural(X).
-fortran(X) :- sat(medium, X), tstatic(X), tweak(X), dgeneric(X), pobject(X), pprocedural(X).
+% languages
 
-% hay que definir todos los lenguajes asi
+assembler(X) :- tuntyped(X), llow(X).
 
+swiprolog(X) :- lhigh(X), tuntyped(X), dspecific(X), plogic(X).
+datalog(X) :- lhigh(X), tuntyped(X), dspecific(X), plogic(X), pquery(X).
+mysql(X) :- lhigh(X), tuntyped(X), dspecific(X), pquery(X).
 
-:- dynamic si/1,no/1.
-sat(Atributo,_) :- (si(Atributo) -> true ; ( no(Atributo) -> fail ; pregunta(Atributo))).
+java(X) :- lmedium(X), tstatic(X), tstrong(X), dgeneric(X), pobject(X), pdeclarative(X).
+fortran(X) :- lmedium(X), tstatic(X), tweak(X), dgeneric(X), pobject(X), pprocedural(X).
+c(x) :- lmedium(X), tstatic(X), tweak(X), dgeneric(X), pprocedural(X).
+cpp(X) :- lmedium(X), tstatic(X), tweak(X), dgeneric(X), pobject(X).
 
-satIf(AtributoNO,AtributoSI,_) :- si(AtributoSI) -> true ; (no(AtributoSI) -> fail ; 
-    (si(AtributoNO) -> fail ; (no(AtributoNO) -> true ; pregunta(AtributoSI)))).
-
-noSat(Atributo,_) :- (si(Atributo) -> fail ; (no(Atributo) -> true ; preguntaNO(Atributo))).
-
-pregunta(A) :- write('¿Are you looking for a language with the following property?: '), write(A), write(' '), nl, read(Resp), nl,
-((Resp == s ; Resp == si ; Resp == sí) -> assert(si(A)); assert(no(A)), fail).
+python(X) :- lhigh(X), dgeneric(X), tdynamic(X), tstrong(X), pfunctional(X), pobject(X).
+erlang(X) :- lhigh(X),  dgeneric(X), tdynamic(X), tstrong(X), pfunctional(X).
+javascript(X) :- lhigh(X), dgeneric(X), tdynamic(X), tweak(X), pobject(X).
+ocaml(X) :- lhigh(X), dgeneric(X), tstatic(X), tstrong(X), pfunctional(X), pobject(X).
+haskell(X) :- lhigh(X), dgeneric(X), tstatic(X), tstrong(X),  pfunctional(X).
+ada(X) :- lhigh(X),  dgeneric(X), tstatic(X), tstrong(X), pprocedural(X).
+scilab(X) :- lhigh(X), dspecific(X), tdynamic(X), tweak(X), pobject(X), pprocedural(X), pfunctional(X).
+r(X) :- lhigh(X), dspecific(X), tdynamic(X), tweak(X), pobject(X), pprocedural(X).
 
 recomienda(assembler,X) :- assembler(X).
-recomienda(python,X) :- python(X).
-recomienda(c, X) :- c(X).
-recomienda(haskell, X) :- haskell(X).
-recomienda(cpp, X) :- cpp(X).
+recomienda(swiprolog, X) :- swiprolog(X).
+recomienda(datalog, X) :- datalog(X).
 recomienda(mysql, X) :- mysql(X).
 recomienda(java, X) :- java(X).
-recomienda(ada, X) :- ada(X).
 recomienda(fortran, X) :- fortran(X).
+recomienda(c, X) :- c(X).
+recomienda(cpp, X) :- cpp(X).
+recomienda(python,X) :- python(X).
 recomienda(erlang, X) :- erlang(X).
-recomienda(scilab, X) :- scilab(X).
 recomienda(javascript, X) :- javascript(X).
-recomienda(r, X) :- r(X).
-recomienda(datalog, X) :- datalog(X).
-recomienda(swiprolog, X) :- swiprolog(X).
 recomienda(ocaml, X) :- ocaml(X).
-
-
-% hay que poner uno por cada lenguaje
-
+recomienda(haskell, X) :- haskell(X).
+recomienda(ada, X) :- ada(X).
+recomienda(scilab, X) :- scilab(X).
+recomienda(r, X) :- r(X).
 recomienda(desconocido, _).
 
-recomendar :- recomienda(Language,_), write('You can use the following languages: '), nl, write(Language), nl. 
+borraResp :- retractall(si(_)), retractall(no(_)).
+
+recomendar :- recomienda(Language,_), write('You can use the following language: '), nl, write(Language), nl, borraResp. 
 % falta implementar un borraResp al final para poder hacer otra consulta despues, esta explicado en la consigna
-% y queda asi recomendar :- recomienda(Language,_), write('You can use the following languages: '), write(Language), nl, borraResp.
+
