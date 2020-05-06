c8024a36ffb9d134810b40a91fc49e35aae437ec
Sebas <zimmermannsebas@gmail.com>
2020-05-06 10:19:48 -0300

    add languages to TP2

--- TP_Prolog/tp2.pl
+++ TP_Prolog/tp2.pl
@@ -1,5 +1,22 @@
-assembler(X) :- sat(low, X), sat(untyped, X).
-python(X) :- sat(functional,X), sat(object,X), sat(tdynamic,X), sat(tstrong,X), sat(generic, python), sat(high,python).
+assembler(X) :- tuntyped(X), sat(low, X).
+python(X) :- sat(high, X), pfunctional(X), pobject(X), dgeneric(X), tdynamic(X), tstrong(X).
+c(x) :- sat(medium,X), tstatic(X), tweak(X), dgeneric(X), pprocedural(X).
+haskell(X) :- sat(high, X), tstatic(X), tstrong(X), dgeneric(X), pure(X).
+erlang(X) :- sat(high, X), tdynamic(X), tstrong(X), dgeneric(X), pfunctional(X).
+ocaml(X) :- sat(high, X), tstatic(X), tstrong(X), dgeneric(X), pfunctional(X), pobject(X).
+scilab(X) :- sat(high, X), tdynamic(X), tweak(X), dspecific(X), pfunctional(X), pobject(X), pprocedural(X).
+swiprolog(X) :- sat(high, X), tuntyped(X), dspecific(X), plogic(X).
+datalog(X) :- sat(high, X), tuntyped(X), dspecific(X), plogic(X), pquery(X).
+mysql(X) :- sat(high, X), tuntyped(X), dspecific(X), pquery(X).
+cpp(X) :- sat(medium, X), tstatic(X), tweak(X), dgeneric(X), pobject(X).
+r(X) :- sat(high, X), tdynamic(X), tweak(X), dspecific(X), pobject(X), pprocedural(X).
+java(X) :- sat(medium, X), tstatic(X), tstrong(X), dgeneric(X), pobject(X), pdeclarative(X).
+javascript(X) :- sat(high, X), tdynamic(X), tweak(X), dgeneric(X), pobject(X).
+ada(X) :- sat(high, X), tstatic(X), tstrong(X), dgeneric(X), pprocedural(X).
+fortran(X) :- sat(medium, X), tstatic(X), tweak(X), dgeneric(X), pobject(X), pprocedural(X).
+
+
+  
 % hay que definir todos los lenguajes asi
 
 
@@ -7,7 +24,7 @@ python(X) :- sat(functional,X), sat(object,X), sat(tdynamic,X), sat(tstrong,X), 
 sat(Atributo,_) :- (si(Atributo) -> true ; ( no(Atributo) -> fail ; pregunta(Atributo))).
 
 pregunta(A) :- write('¿Are you looking for a language with the following property?: '), write(A), write(' '), nl, read(Resp), nl,
-((Resp == s ; Resp == si ; Resp == sí) -> assert(si(A)); assert(no(A))), fail.
+((Resp == s ; Resp == si ; Resp == sí) -> assert(si(A)); assert(no(A)), fail).
 
 recomienda(python,X) :- python(X).
 recomienda(assembler,X) :- assembler(X).
